24d5fb75e72199000a6337abdf1d842ec64cec2e
00Dedsec <631038893@qq.com>
2024-05-08 17:15:48 +0800

    go mod test

---
+++ calculator/calc.go
@@ -0,0 +1,17 @@
+package calculator
+
+func Add(x, y int) int {
+	return x + y
+}
+
+func Sub(x, y int) int {
+	return x - y
+}
+
+func Multi(x, y int) int {
+	return x * y
+}
+
+func Div(x, y int) int {
+	return x / y
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"calc/calculator"
+	"fmt"
+)
+
+func main() {
+	add := calculator.Add(1, 1)
+	fmt.Println(add)
+}
